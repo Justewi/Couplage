13256f14fecc18c1830b155484d05843eee5965e
Cyprien Levy <cyprien.levy@etu.unice.fr>
2017-04-29 18:10:27 +0200

    modification du main

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@
 
 
 
-enum TypeGraph {
+enum TypeDeGraph {
     GENERAL, BIPARTI
 };
 
@@ -22,57 +22,57 @@ enum TypeGraph {
 int main(int argc, char** argv) {
     // - - - - - Argument parsing
     if (argc < 4 || argc > 8) {
-        std::cerr << "Invalid amount of parameters." << std::endl
-                << "Usage: " << argv[0] << " Type_of_graph Number_of_nodes step [min max max_thread nb_repeat -p]" << std::endl;
+        std::cerr << "Le nombre de paramètre est incorrect" << std::endl
+                << "Utilisation : " << argv[0] << " Type_de_graph Nombre_de_noeud pas [minimum maximum nb_repetition -p]" << std::endl;
         return 0;
     }
 
-    TypeGraph type;
+    TypeGraph type_de_graph;
     if ((std::strcmp(argv[1], "g") == 0) || (std::strcmp(argv[1], "general") == 0)) {
-        type = TypeGraph::GENERAL;
+        type_de_graph = TypeDeGraph::GENERAL;
     } else if ((std::strcmp(argv[1], "b") == 0) || (std::strcmp(argv[1], "biparti") == 0)) {
-        type = TypeGraph::BIPARTI;
+        type_de_graph = TypeDeGraph::BIPARTI;
     } else {
-        std::cerr << "Invalid value for Type_of_graph: " << argv[1] << ". Expected (g | general) for general graph or (b | biparti) for bipartite graph." << std::endl;
+        std::cerr << "Type de graph inconnu: " << argv[1] << ". Soit  g pour un graph general, soit b pour un graph biparti." << std::endl;
         return 1;
     }
 
-    unsigned int nbNodes = std::atoi(argv[2]);
-    if (nbNodes <= 0) {
-        std::cerr << "Invalid value for Number_of_nodes: " << nbNodes << ". Expected a positive integer." << std::endl;
+    unsigned int nombre_de_noeu = std::atoi(argv[2]);
+    if (nombre_de_noeu <= 0) {
+        std::cerr << "le nombre de noeud ne peut pas etre négatif: " << nombre_de_noeu << std::endl;
         return 1;
     }
-    float step = std::atof(argv[3]);
-    if (step <= 0 || step > 1) {
-        std::cerr << "Valeur invalide pour le nombre d'étapes : " << step << ". Valeur attendue entre 0 et 1." << std::endl;
+
+    float pas = std::atof(argv[3]);
+    if (pas <= 0 || pas > 1) {
+        std::cerr << "Valeur invalide pour le nombre d'étapes : " << pas << ". doit etre comprise entre 0 et 1." << std::endl;
         return 1;
     }
+
     float min = 0;
     if (argc > 4) {
         min = std::atof(argv[4]);
         if (min < 0 || min >= 1) {
-            std::cerr << "Invalid value for min: " << min << ". Expected a decimal value in [0;1)." << std::endl;
+            std::cerr << "valeur du minimum incorrecte: " << min << ". doit etre comprise entre 0 et 1." << std::endl;
             return 1;
         }
     }
+
     float max = 1;
     if (argc > 5) {
         max = std::atof(argv[5]);
         if (max <= 0 || max > 1) {
-            std::cerr << "Invalid value for max: " << max << ". Expected a decimal value in (0;1]." << std::endl;
+            std::cerr << "valeur du maximum incorrecte: " << max << ". doit etre comprise entre 0 et 1." << std::endl;
             return 1;
         }
     }
-
-
     
-
     unsigned int repetition = 100;
     if (argc > 6) {
         repetition = std::atoi(argv[6]);
         if (repetition < 0) {
-            std::cerr << "Invalid value for nb_repeat: " << repetition
-                    << ". Expected a positive value." << std::endl;
+            std::cerr << "Inombre de repetition incorrect: " << repetition
+                    << ". doit etre positif." << std::endl;
             return 1;
         }
     }
@@ -80,24 +80,24 @@ int main(int argc, char** argv) {
 
 
     // - - - - - Actual work
-    std::vector<std::pair<float, unsigned int>> results ( (max - min) / step, std::pair<float, unsigned int>(0, 0));
+    std::vector<std::pair<float, unsigned int>> results ( (max - min) / pas, std::pair<float, unsigned int>(0, 0));
 
-    for (unsigned int i = 0; (i + 1) * step + min <= max; i++) {
-        results[i].first = (i + 1) * step + min;
+    for (unsigned int i = 0; (i + 1) * pas + min <= max; i++) {
+        results[i].first = (i + 1) * pas + min;
   
         float proba = results[i].first;
         std::random_device rand;
     	unsigned int& result = std::ref(results[i].second);
     	result= 0;
 	    for (unsigned int i = 0; i < repetition; i++) {
-	        if (type == BIPARTI) {
-	            UnDiGraph graph = RandomGraphBuilder::RandomBipartiteGraph(nbNodes, nbNodes,proba, rand);
-	            if (GraphMatching::CouplageParfait(graph, GraphMatching::CouplageMaximumBiparti(graph, nbNodes))) {
+	        if (type_de_graph == BIPARTI) {
+	            UnDiGraph graph = RandomGraphBuilder::RandomBipartiteGraph(nombre_de_noeu, nombre_de_noeu,proba, rand);
+	            if (GraphMatching::CouplageParfait(graph, GraphMatching::CouplageMaximumBiparti(graph, nombre_de_noeu))) {
 	                result++;
 	            }
 	        } else {
-	            UnDiGraph graph = RandomGraphBuilder::RandomGraph(nbNodes,proba, rand);
-	            if (GraphMatching::IsPerfectMatch(graph, GraphMatching::MaximumMatching(graph))) {
+	            UnDiGraph graph = RandomGraphBuilder::RandomGraph(nombre_de_noeu,proba, rand);
+	            if (GraphMatching::CouplageParfait(graph, GraphMatching::CouplageMaximumGeneral(graph))) {
 	                result++;
 	            }
 	        }
@@ -109,8 +109,8 @@ int main(int argc, char** argv) {
 
 
     std::cout << " ################ Résultats ###############" << std::endl;
-    std::cout <<"Nombre de nodes : " << nbNodes << std::endl;
-    std::cout << "Nombre de répétitions : "<< repetition << ", Pas :" << step << ", min : " << min << ", max :" << max << std::endl;
+    std::cout <<"Nombre de nodes : " << nombre_de_noeu << std::endl;
+    std::cout << "Nombre de répétitions : "<< repetition << ", Pas :" << pas << ", min : " << min << ", max :" << max << std::endl;
 
     for (std::pair<float, unsigned int> i : results) {
         std::cout << i.first << "," << i.second << "," << ((float) i.second / repetition) << std::endl;
